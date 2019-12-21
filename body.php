c2fc02cb929afe082ea87e40ee2f633bc4a4316f
Jirayu <jikull55@gmail.com>
2019-12-21 15:53:38 +0700

    เอาโปรเจคเข้า git

---
+++ body.php
@@ -0,0 +1,138 @@
+
+   <div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/banner3.jpg" alt="Los Angeles" style="width:100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="img/banner2.jpg" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="img/banner4.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner1.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner3.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+		
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+<!-- 							<h3 class="title">New Products</h3> -->
+<!-- 							<div class="section-nav"> --> 
+<!-- 								<ul class="section-tab-nav tab-nav"> -->
+<!-- 									<li class="active"><a data-toggle="tab" href="#tab1">Laptops</a></li> -->
+<!-- 									<li><a data-toggle="tab" href="#tab1">Smartphones</a></li> -->
+<!-- 									<li><a data-toggle="tab" href="#tab1">Cameras</a></li> -->
+<!-- 									<li><a data-toggle="tab" href="#tab1">Accessories</a></li> -->
+<!-- 								</ul> -->
+				</div> 
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					
+					<!-- Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- HOT DEAL SECTION -->
+
+		<!-- /HOT DEAL SECTION -->
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab2" class="tab-pane fade in active">
+									<div class="products-slick" data-nav="#slick-nav-2">
+										<!-- product -->
+										
+										
+										<!-- /product -->
+									</div>
+									<div id="slick-nav-2" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- /Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+		
+		
